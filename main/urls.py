627c89b21b216e65aa8d511a8e7c4153ac5eea2e
serzhan29 <tserzhan1@gmail.com>
2025-04-13 21:44:14 +0500

    Добавление новой ссылки (indicator_report_view) и убрали старые и не нужные ссылки

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import  DirectionListView, YearListView, TeacherReportView, UpdateValueView
 from .views_docx import download_teacher_report
 from .views_display import reports_dashboard, update_report2
-from .observer import teachers_by_faculty, TeacherReportReadOnlyView
+from .observer import teachers_by_faculty, TeacherReportReadOnlyView, indicator_report_view
 
 urlpatterns = [
     path('', DirectionListView.as_view(), name='direction_list'),
@@ -15,6 +15,7 @@ urlpatterns = [
     path('teacher/', teachers_by_faculty, name='teachers_by_faculty'),
 
     path('teacher/report/', TeacherReportReadOnlyView.as_view(), name='teacher_report_readonly'),
+    path('report/', indicator_report_view, name='indicator_report'),
 
 ]
 
