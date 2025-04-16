8e0a6b85d790f8a347ed449ea3f950e6919c0430
serzhan29 <tserzhan1@gmail.com>
2025-04-17 02:40:43 +0500

    Добавление динамического обновления кафедры при выборе факультета

--- app/settings.py
+++ app/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_select2',
+    'smart_selects',
 
     'main',
     'user',
--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
     path('user/', include("user.urls")),
+    path('chaining/', include('smart_selects.urls')),
 
 ]
