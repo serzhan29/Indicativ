aed8c0e0df040bf1d07240f830b2c3ecb6dd8f74
serzhan29 <tserzhan1@gmail.com>
2025-05-16 22:55:05 +0500

    Добавление media для хранения файлов

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('chaining/', include('smart_selects.urls')),
 
 ] + debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
